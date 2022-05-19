017360c47acd12a14a9644fdd7d710200b31426b
amantarento <amankumar.sharma@tarento.com>
2022-05-19 16:00:30 +0530

    chnages for the username and config

--- projects/discussion-ui/src/lib/pipes/pipes.module.ts
+++ projects/discussion-ui/src/lib/pipes/pipes.module.ts
@@ -5,13 +5,13 @@ import { SortByPipe } from './sort-by/sort-by.pipe';
 import { PipeRelativeTimePipe } from './pipe-relative-time/pipe-relative-time.pipe';
 import { SplitInitialsPipe } from './split-initials/split-initials.pipe';
 import { PipeListFilterPipe } from './pipe-list-filter/pipe-list-filter.pipe';
-
+import {CreateInitialsPipe} from './create-initials/create-initials.pipes'
 
 @NgModule({
-  declarations: [PipeFilterPipe, SortByPipe, PipeRelativeTimePipe, SplitInitialsPipe, PipeListFilterPipe],
+  declarations: [PipeFilterPipe, SortByPipe, PipeRelativeTimePipe, SplitInitialsPipe, PipeListFilterPipe,CreateInitialsPipe],
   imports: [
     CommonModule
   ],
-  exports: [PipeFilterPipe, SortByPipe, PipeRelativeTimePipe, SplitInitialsPipe, PipeListFilterPipe]
+  exports: [PipeFilterPipe, SortByPipe, PipeRelativeTimePipe, SplitInitialsPipe, PipeListFilterPipe,CreateInitialsPipe]
 })
 export class PipesModule { }
